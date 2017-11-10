7fe52f9dbdd889b5eae23a22bd6586842abb3ac3
Wisner Júnior <wisnerjr@gmail.com>
2017-11-10 11:34:49 -0200

    1002 to 1009

---
+++ beginner/1008_salary.py
@@ -0,0 +1,8 @@
+worker_number = int(input())
+hours_worked = int(input())
+salary_per_hour = float(input())
+
+salary = hours_worked * salary_per_hour
+
+print("NUMBER = %i" %worker_number)
+print("SALARY = U$ %0.2f" %salary)
---
+++ beginner/1009_salary_with_bonus.py
@@ -0,0 +1,7 @@
+vendor_name = input()
+salary = float(input())
+total_sold = float(input())
+
+total = salary + (total_sold * 0.15)
+
+print("TOTAL = R$ %0.2f" %total)
